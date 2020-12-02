0623cb9094f9342fb8f581a3e89b5cc7e3c5b5eb
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-12-01 18:26:46 -0800

    create 5 questions in the html form and add styling to css

--- src/js/server.js
+++ src/js/server.js
@@ -1,56 +1,56 @@
-// Necessary Imports
-var express = require('express');
-var mysql = require('mysql');
-var app = express();
+// // Necessary Imports
+// var express = require('express');
+// var mysql = require('mysql');
+// var app = express();
 
-// Connection Config
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "epicodus",
-  database: "mydb"
-});
+// // Connection Config
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "epicodus",
+//   database: "mydb"
+// });
 
-// Connection
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// // Connection
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
-// Get From Database
-app.get('/', function(req, resp) {
-  connection.query("SELECT * FROM aboutuser", function(error, rows, fields) {
-    if(!!error) {
-      console.log('error get query');
-      console.log(error); 
-    } else {
-      console.log('success get query');
-    }
-  });
-})
+// // Get From Database
+// app.get('/', function(req, resp) {
+//   connection.query("SELECT * FROM aboutuser", function(error, rows, fields) {
+//     if(!!error) {
+//       console.log('error get query');
+//       console.log(error); 
+//     } else {
+//       console.log('success get query');
+//     }
+//   });
+// })
 
-// Send To Database
-// Select...send ${gender} .......send that to mydb.quiz_one
-var sql = "INSERT INTO aboutuser (gender) VALUES ?";
-var values = [
-  ['Male', "21", "no", "highschool", "full-time"],
-  ['Male', "58", "yes", "college", "part-time"],
-  ['Male'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female'],
-  ['Female']
-];
-connection.query(sql, [values], function (err, result) {
-  if (err) throw err;
-  console.log("Number of records inserted: " + result.affectedRows);
-});
+// // Send To Database
+// // Select...send ${gender} .......send that to mydb.quiz_one
+// var sql = "INSERT INTO aboutuser (gender) VALUES ?";
+// var values = [
+//   ['Male', "21", "no", "highschool", "full-time"],
+//   ['Male', "58", "yes", "college", "part-time"],
+//   ['Male'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female'],
+//   ['Female']
+// ];
+// connection.query(sql, [values], function (err, result) {
+//   if (err) throw err;
+//   console.log("Number of records inserted: " + result.affectedRows);
+// });
 
-app.listen(8080);
+// app.listen(8080);
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  $("#response").click(function(event) {
+  $("#userResponse").click(function(event) {
     event.preventDefault();
     let gender = $("input:radio:checked").val();
     console.log(`The user input is: ${gender}`);
